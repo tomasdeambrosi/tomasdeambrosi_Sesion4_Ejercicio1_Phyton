b54b5736a6bb4312e6b317f95d0572e558255f51
tomasdeambrosi <tomasdeambrosi@gmail.com>
2022-03-30 00:29:55 -0300

    Add files via upload

---
+++ tomasdeambrosi_Sesion4_Ejercicio1_Phyton.py
@@ -0,0 +1,8 @@
+#Escribe un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
+
+edad = int(input('¿Cuál es tu edad?'))
+
+if edad >= 18:
+    print('Sos mayor de edad')
+else:
+    print('No sos mayor de edad')
